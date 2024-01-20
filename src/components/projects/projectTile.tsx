c3fef12bb188ceade40b139f8faf21782b3719da
Renzo Reccio <reccio_2001@hotmail.com>
2024-01-20 11:07:10 -0500

    Fix project link

--- src/components/projects/projectTile.tsx
+++ src/components/projects/projectTile.tsx
@@ -12,7 +12,7 @@ export default function ProjectTile(props: ProjectTileProps) {
   const { title, blurb, link, id } = props
 
   return (
-    <Link href={"/"} data-testid={"project-tile-" + id} className="text-zinc-700 dark:text-zinc-400 
+    <Link href={link}  target="_blank" data-testid={"project-tile-" + id} className="text-zinc-700 dark:text-zinc-400 
     hover:text-emerald-500 dark:hover:border-zinc-700 shadow-sm w-full md:w-1/3 rounded-lg p-6">
       <h2 className="text-zinc-900 hover:text-zinc-900 dark:text-zinc-100 font-semibold py-4">{title}</h2>
       <p className="text-zinc-900 hover:text-zinc-900 dark:text-zinc-400">{blurb.slice(0, 150)}</p>
