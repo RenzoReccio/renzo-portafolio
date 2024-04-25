a6b93482854b6633eaf21d3841d016a849e69a4d
Renzo Reccio <reccio_2001@hotmail.com>
2024-04-25 09:34:51 -0500

    fix for dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 'use client'
-import Layout from '@/components/layout/layout'
 import { useEffect } from 'react'
 import HomeComponent from '@/components/home'
 
 
 export default function Home() {
-
-  useEffect(() => {
-    if (localStorage.theme === "dark"){
-      document.documentElement.classList.add('dark')
-    }
-  }, [])
 
   return (
       <HomeComponent />
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import Footer from "./footer";
-import Header from "./header";
 import Menu from "./menu";
 import StickyHeader from "./stickyHeader";
 
@@ -15,19 +14,23 @@ export default function Layout(props: LayoutProps) {
   const [showMenu, setShowMenu] = useState<boolean>(false)
   const [offset, setOffset] = useState(0);
 
-    useEffect(() => {
-        const onScroll = () => setOffset(window.pageYOffset);
-        // clean up code
-        window.removeEventListener('scroll', onScroll);
-        window.addEventListener('scroll', onScroll, { passive: true });
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
+  useEffect(() => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.add('dark')
+    }
+
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
     <div className=" w-screen min-h-screen bg-gray-50 dark:bg-black  items-center flex flex-col"/* " overflow-hidden flex flex-col items-center w-screen bg-gray-50" */ data-testid="layout-div">
-      <StickyHeader offset={offset} showMenu={showMenu} setShowMenu={() => setShowMenu(!showMenu)}/>
-      {showMenu 
-      && <Menu setShowMenu={(arg) => setShowMenu(arg)}/>}
+      <StickyHeader offset={offset} showMenu={showMenu} setShowMenu={() => setShowMenu(!showMenu)} />
+      {showMenu
+        && <Menu setShowMenu={(arg) => setShowMenu(arg)} />}
       <div className={showMenu ? "text-black dark:bg-zinc-900 blur saturate-50 w-screen body-width bg-white overflow-hidden" : " dark:bg-zinc-900 w-screen body-width  bg-white"} data-testid="main-div">
         {children}
       </div>
